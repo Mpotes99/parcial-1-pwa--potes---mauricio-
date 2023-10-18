ec00ee5a289874deb3cbba1e6576f098745cb567
Mauricio Potes <potesmauricio99@gmail.com>
2023-10-17 22:10:15 -0300

    act

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
-
-const app = new VTTCue({
-    el:'#app',
-    data:{
-        personajes:[],
-        personajeSeleccionado:{},
-        mostrarModal:false
+new Vue({
+    el: '#app',
+    data: {
+      personajes: [],
+      personajeSeleccionado: null,
     },
-    methods:{
-        obtenerPersonajes() {
-            fetch('https://rickandmortyapi.com/api/character/?status=alive')
-                .then(response => response.json())
-                .then(data => {
-                    this.personajes = data.results;
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        },
-        mostrarDetalles(personaje) {
-            this.personajeSeleccionado = personaje;
-            this.mostrarModal = true;
-        },
-        cerrarModal() {
-            this.mostrarModal = false;
-        }
-
+    created() {
+      this.obtenerPersonajes();
     },
-    mounted(){
-        this.obtenerPersonajes();
-    }
-})
+    methods: {
+      obtenerPersonajes() {
+        const apiUrl = 'https://rickandmortyapi.com/api/character/?status=alive';
+        fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+            this.personajes = data.results.slice(0, 20);
+          })
+          .catch(error => console.error('Error:', error));
+      },
+      abrirModal(personaje) {
+        this.personajeSeleccionado = personaje;
+      },
+      cerrarModal() {
+        this.personajeSeleccionado = null;
+      },
+    },
+  });
+  
