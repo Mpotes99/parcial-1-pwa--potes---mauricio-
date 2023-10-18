d55d4752929f0673e67f6abba7ba43b29dd0e7e8
Mauricio Potes <potesmauricio99@gmail.com>
2023-10-17 21:42:01 -0300

    act

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const jsonConverter = (response) => response.json();
 
 const app = new VTTCue({
     el:'#app',
     data:{
         personajes:[],
-        personajeselec:{}
+        personajeSeleccionado:{},
+        mostrarModal:false
     },
     methods:{
-        obtenerPersonajes(){
+        obtenerPersonajes() {
             fetch('https://rickandmortyapi.com/api/character/?status=alive')
-            .then(respuesta => respuesta.json())
-            .then(data => {
-                this.personajes = data.results;
-            })
-            .catch(error => {
-                console.error('Error', error);
-            })
+                .then(response => response.json())
+                .then(data => {
+                    this.personajes = data.results;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
         },
-    mostrarDetalles(personaje){
-        this.personajeSeleccionado = personaje;
-        this.mostrasModal = true;
-    },
-    cerrarModal(){
-        this.mostrarModal = false;
-    }
+        mostrarDetalles(personaje) {
+            this.personajeSeleccionado = personaje;
+            this.mostrarModal = true;
+        },
+        cerrarModal() {
+            this.mostrarModal = false;
+        }
 
     },
     mounted(){
